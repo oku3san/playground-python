212aa58d16aba335536c75a831ff4231efbd7f8e
oku3san <takaya0303@gmail.com>
2023-11-10 21:00:00 +0900

    Update

--- for.py
+++ for.py
@@ -19,9 +19,42 @@
 #         active_users[user] = status
 
 # ========
-for i in range(5):
-    print(i)
 
-print(list(range(5, 10)))
-print(list(range(0, 10, 3)))
-print(list(range(-10, -100, -30)))
+# for i in range(5):
+#     print(i)
+#
+# print(list(range(5, 10)))
+# print(list(range(0, 10, 3)))
+# print(list(range(-10, -100, -30)))
+
+# =====
+
+# a = ['Mary', 'had', 'a', 'little', 'lamb']
+# for i in range(len(a)):
+#     print(i, a[i])
+
+# =====
+
+# for n in range(2, 10):
+#     print('n=', n)
+#     for x in range(2, n):
+#         print('x=', x)
+#         if n % x == 0:
+#             print(n, 'equals', x, '*', n // x)
+#             break
+#     else:
+#         # loop fell through without finding a factor
+#         print(n, 'is a prime number')
+
+# =====
+
+def fib(n):
+    """Print a Fibonacci series up to n."""
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a + b
+    print()
+
+
+fib(2000)
